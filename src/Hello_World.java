8d9dc66ce6c944a89ba7e0efc2c5ce12190688f5
clplaneguy <clplaneguy@gmail.com>
2018-07-02 12:14:34 -0500

    dfgh

---
+++ src/Hello_World.java
@@ -0,0 +1,13 @@
+import lejos.hardware.*;
+
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!!");
+        System.out.println("2/24/2018");
+        System.out.println("Press any key ...");
+
+        Button.waitForAnyPress();
+    }
+}
